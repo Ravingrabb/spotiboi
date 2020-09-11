b2d881c91fd2abb6826455578a13a799debdf614
Ravingrabb <dog1995@alumni.nottingham.ac.uk>
2020-09-12 02:57:58 +0500

    new start page

--- app.py
+++ app.py
@@ -1,19 +1,4 @@
 """
-Prerequisites
-    pip3 install spotipy Flask Flask-Session
-    // from your [app settings](https://developer.spotify.com/dashboard/applications)
-    export SPOTIPY_CLIENT_ID=client_id_here
-    export SPOTIPY_CLIENT_SECRET=client_secret_here
-    export SPOTIPY_REDIRECT_URI='http://127.0.0.1:8080' // must contain a port
-    // SPOTIPY_REDIRECT_URI must be added to your [app settings](https://developer.spotify.com/dashboard/applications)
-    OPTIONAL
-    // in development environment for debug output
-    export FLASK_ENV=development
-    // so that you can invoke the app outside of the file's directory include
-    export FLASK_APP=/path/to/spotipy/examples/app.py
- 
-    // on Windows, use `SET` instead of `export`
-Run app.py
     python3 -m flask run --port=8080
     NOTE: If receiving "port already in use" error, try other ports: 5000, 8090, 8888, etc...
         (will need to be updated in your Spotify app and SPOTIPY_REDIRECT_URI variable)
@@ -32,8 +17,6 @@ import time
 from datetime import datetime
 import logging
 
-
-UPDATE_JOB = None
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
