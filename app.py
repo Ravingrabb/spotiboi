b1ffefbeec18c4e23d39f81e7a7c8e7a5655a70c
Ravingrabb <dog1995@alumni.nottingham.ac.uk>
2020-09-15 01:57:57 +0500

    рататата

--- app.py
+++ app.py
@@ -38,7 +38,8 @@ else:
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
 #расписания
-scheduler = Scheduler(connection=Redis(host="192.168.0.101")) # Get a scheduler for the "default" queue
+#scheduler = Scheduler(connection=Redis(host="192.168.0.101")) # Get a scheduler for the "default" queue
+scheduler = Scheduler(connection=Redis()) # Get a scheduler for the "default" queue
 
 
 
